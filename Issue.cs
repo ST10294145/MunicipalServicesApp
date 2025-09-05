acbf49025d28401171115f171e0db84028717929
Saihil Gurupersad <ST10294145@vcconnect.edu.za>
2025-09-05 19:23:24 +0200

    Made it a public class

---
+++ Issue.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MunicipalServicesApp
+{
+    public class Issue
+    {
+        public int Id { get; set; }
+        public string ReporterName { get; set; }
+        public string Email { get; set; }
+        public string Category { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; } = "Received"; // Default status
+    }
+}

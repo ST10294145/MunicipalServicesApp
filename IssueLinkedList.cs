fa9eebdca54a3675abb09aee7a1f7b339f1b40d0
Saihil Gurupersad <ST10294145@vcconnect.edu.za>
2025-09-05 19:23:44 +0200

    Added this list for issue class

---
+++ IssueLinkedList.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MunicipalServicesApp
+{
+    // Node in the linked list
+    public class IssueNode
+    {
+        public Issue Data { get; set; }
+        public IssueNode Next { get; set; }
+
+        public IssueNode(Issue data)
+        {
+            Data = data;
+            Next = null;
+        }
+    }
+
+    // Custom linked list to store issues
+    public class IssueLinkedList
+    {
+        private IssueNode head;
+        private int counter = 0;
+
+        // Add a new issue to the list
+        public void AddIssue(Issue issue)
+        {
+            counter++;
+            issue.Id = counter; // auto-generate issue ID
+
+            IssueNode newNode = new IssueNode(issue);
+
+            if (head == null)
+            {
+                head = newNode;
+            }
+            else
+            {
+                IssueNode current = head;
+                while (current.Next != null)
+                {
+                    current = current.Next;
+                }
+                current.Next = newNode;
+            }
+        }
+
+        // Retrieve all issues as IEnumerable
+        public IEnumerable<Issue> GetAllIssues()
+        {
+            IssueNode current = head;
+            while (current != null)
+            {
+                yield return current.Data;
+                current = current.Next;
+            }
+        }
+    }
+}

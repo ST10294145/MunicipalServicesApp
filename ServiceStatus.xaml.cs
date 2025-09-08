9f1508163cca0725c7694fd151acf01b715aee0d
Saihil Gurupersad <ST10294145@vcconnect.edu.za>
2025-09-08 09:25:42 +0200

    Added logic to ServiceStatus.xaml.cs

--- ServiceStatus.xaml.cs
+++ ServiceStatus.xaml.cs
@@ -1,27 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace MunicipalServicesApp
 {
-    /// <summary>
-    /// Interaction logic for ServiceStatus.xaml
-    /// </summary>
     public partial class ServiceStatus : Window
     {
         public ServiceStatus()
         {
             InitializeComponent();
+            LoadIssues();
+        }
+
+        private void LoadIssues()
+        {
+            // Pull issues from the global linked list
+            var issues = ((App)Application.Current).IssueList.GetAllIssues().ToList();
+
+            dgIssues.ItemsSource = issues;
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow main = new MainWindow();
+            main.Show();
+            this.Close();
         }
     }
 }

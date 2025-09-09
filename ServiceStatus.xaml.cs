d45c5e22d21d34d2613de195d86e14be1f276723
Saihil Gurupersad <ST10294145@vcconnect.edu.za>
2025-09-09 10:40:24 +0200

    removed old code from class

--- ServiceStatus.xaml.cs
+++ ServiceStatus.xaml.cs
@@ -7,23 +7,7 @@ namespace MunicipalServicesApp
     {
         public ServiceStatus()
         {
-            InitializeComponent();
-            LoadIssues();
-        }
 
-        private void LoadIssues()
-        {
-            // Pull issues from the global linked list
-            var issues = ((App)Application.Current).IssueList.GetAllIssues().ToList();
-
-            dgIssues.ItemsSource = issues;
-        }
-
-        private void Back_Click(object sender, RoutedEventArgs e)
-        {
-            MainWindow main = new MainWindow();
-            main.Show();
-            this.Close();
         }
     }
 }
